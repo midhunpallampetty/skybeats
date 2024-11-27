5522af2cba888baead9528f4b2155ef230143b82
midhunpallampetty <midhunpallampetty@gmai.com>
2024-11-27 14:11:05 +0530

    after production changes in cookies

--- pages/api/utils/adminAxiosInstance.ts
+++ pages/api/utils/adminAxiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const adminAxios = axios.create({
-  baseURL: process.env.API_ENDPOINT, // Update this to match your API base URL
+  baseURL: "https://www.skybeats.site/api/", // Update this to match your API base URL
   withCredentials: true,
 });
 
