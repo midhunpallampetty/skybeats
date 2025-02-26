57d05307148d810c7734ba30f0bc6b6fa2c1f0e9
MIDHUN <132975846+midhunpallampetty@users.noreply.github.com>
2025-02-26 12:43:01 +0530

    Update apolloClient.ts

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 const   client=new ApolloClient({
-    uri:'https://skybeats.neptunemusics.shop/graphql',
+    uri:'https://skybeats.site/graphql',
     cache:new InMemoryCache(),
 });
 export default client;
