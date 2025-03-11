905b0e4a7b4ded246ea8b81089dc9c12e2b1f1e6
midhunpallampetty <midhunpallampetty@gmai.com>
2025-03-12 00:44:06 +0530

    changes in career page

--- pages/api/tokenVerify.ts
+++ pages/api/tokenVerify.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 const getUsers = async (req: NextApiRequest, res: NextApiResponse) => {
-    const graphQLClient = new GraphQLClient("http://localhost:3300/graphql");
+    const graphQLClient = new GraphQLClient("https://www.skybeats.site/graphql");
 
     const query = gql`
         query isAuthorised($token: String!) {
